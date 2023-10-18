3753d824c8a7ee540fef937c62c3c7478b483097
nifarn <nifarn@microsoft.com>
2023-10-18 16:17:10 -0700

    created simple bash scripts to run experiments

--- scripts/evaluate_gpt35turbo.sh
+++ scripts/evaluate_gpt35turbo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-output_dir=$1
+output_dir="output/gpt-3.5"
 
-python -m tooltalk.evaluate.evaluate_openai
-python -m tooltalk.evaluate.evaluate_openai
-python -m tooltalk.evaluate.calculate_error_types
-python -m tooltalk.evaluate.calculate_error_types
+python -m tooltalk.evaluate.evaluate_openai --dataset data/easy --model gpt-3.5-turbo --output_dir $output_dir/easy
+python -m tooltalk.evaluate.evaluate_openai --dataset data/tooltalk --model gpt-3.5-turbo --output_dir $output_dir/hard
+python -m tooltalk.evaluate.calculate_error_types --dataset $output_dir/easy --metrics $output_dir/easy_metrics.json
+python -m tooltalk.evaluate.calculate_error_types --dataset $output_dir/hard --metrics $output_dir/hard_metrics.json
